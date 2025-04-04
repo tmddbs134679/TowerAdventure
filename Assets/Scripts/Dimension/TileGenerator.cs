6b43b8301d8b9938b60004e000d0eb83593b4f48
송승윤 <119779039+tmddbs134679@users.noreply.github.com>
2025-04-04 14:03:57 +0900

    StageSelectScene, Stage1Scene 생성

--- Assets/Scripts/Dimension/TileGenerator.cs
+++ Assets/Scripts/Dimension/TileGenerator.cs
@@ -11,6 +11,7 @@ public class TileGenerator : EditorWindow
     GameObject tilePrefab;
     int rows = 5;
     int cols = 5;
+    int height = 3;
     float spacing = 1f;
 
     [MenuItem("Tools/Tile Grid Generator")]
@@ -24,6 +25,7 @@ public class TileGenerator : EditorWindow
         tilePrefab = (GameObject)EditorGUILayout.ObjectField("Tile Prefab", tilePrefab, typeof(GameObject), false);
         rows = EditorGUILayout.IntField("Rows", rows);
         cols = EditorGUILayout.IntField("Cols", cols);
+        height = EditorGUILayout.IntField("Heights", height);
         spacing = EditorGUILayout.FloatField("Spacing", spacing);
 
         if (GUILayout.Button("Generatge"))
@@ -44,15 +46,18 @@ public class TileGenerator : EditorWindow
 
         GameObject parent = new GameObject("TileGrid");
 
-        for(int y = 0; y< rows; y++)
+        for (int y = 0; y < height; y++) // Y축 쌓기
         {
-            for(int x = 0; x< cols; x++)
+            for (int z = 0; z < rows; z++) // 세로
             {
-                Vector3 pos = new Vector3(x * spacing, 0, y * spacing);
+                for (int x = 0; x < cols; x++) // 가로
+                {
+                    Vector3 pos = new Vector3(x * spacing, y * spacing, z * spacing);
 
-                GameObject instance = (GameObject)PrefabUtility.InstantiatePrefab(tilePrefab, SceneManager.GetActiveScene());
-                instance.transform.position = pos;
-                instance.transform.SetParent(parent.transform);
+                    GameObject instance = (GameObject)PrefabUtility.InstantiatePrefab(tilePrefab, SceneManager.GetActiveScene());
+                    instance.transform.position = pos;
+                    instance.transform.SetParent(parent.transform);
+                }
             }
         }
     }

c8a172654b0c3a3bb519e687ed533800bda11c78
송승윤 <119779039+tmddbs134679@users.noreply.github.com>
2025-06-04 23:58:03 +0900

    feat : 1. 어드레서블 작업 준비 및 Test 2. ResourceManager 작업

--- Assets/02.Scripts/Managers/Core/ResourceManager.cs
+++ Assets/02.Scripts/Managers/Core/ResourceManager.cs
@@ -1,8 +1,88 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.AsyncOperations;
+using System;
+using Object = UnityEngine.Object;
 
 public class ResourceManager
 {
-  
+    Dictionary<string, UnityEngine.Object> _resources = new Dictionary<string, UnityEngine.Object>();
+
+
+    public T Load<T>(string key) where T : Object
+    {
+        if (_resources.TryGetValue(key, out Object resource))
+            return resource as T;
+
+        return null;
+    }
+
+    public void LoadAsync<T>(string key, Action<T> callback = null) where T : UnityEngine.Object
+    {
+        if(_resources.TryGetValue(key, out Object resource))
+        {
+            callback?.Invoke(resource as T);
+            return;
+        }
+
+        var asyncOperation =  Addressables.LoadAssetAsync<T>(key);
+        asyncOperation.Completed += (op) =>
+        {
+            _resources.Add(key, op.Result);
+            callback?.Invoke(op.Result);
+        };
+
+    }
+
+    public void LoadAllAsync<T>(string label, Action<string, int ,int> callback) where T : UnityEngine.Object
+    {
+        var OPHandle = Addressables.LoadResourceLocationsAsync(label, typeof(T));
+        OPHandle.Completed += (op) =>
+        {
+            int loadCount = 0;
+            int totalCount = op.Result.Count;
+
+            foreach (var result in op.Result)
+            {
+                LoadAsync<T>(result.PrimaryKey,(obj) =>
+                {
+                    loadCount++;
+                    callback?.Invoke(result.PrimaryKey, loadCount, totalCount);
+                });
+            }
+        };
+    }
+
+    public GameObject Instantiate(string key, Transform parent = null, bool pooling = false)
+    {
+        GameObject prefab = Load<GameObject>($"{key}");
+        if (prefab == null)
+        {
+            Debug.LogError($"Failed to load prefab : {key}");
+            return null;
+        }
+
+        //if (pooling)
+        //    return Managers.Pool.Pop(prefab);
+
+        GameObject go = Object.Instantiate(prefab, parent);
+
+        go.name = prefab.name;
+        return go;
+    }
+
+    public void Destroy(GameObject go)
+    {
+        if (go == null)
+            return;
+
+        //if (Managers.Pool.Push(go))
+        //    return;
+
+        Object.Destroy(go);
+    }
+
+
 }
